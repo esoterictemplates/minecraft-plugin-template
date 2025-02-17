893bd2bd59b51e86e956895a72dbd94ef1910ee0
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-17 18:27:39 +0000

    Add missing test

--- project/src/test/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrapTest.kt
+++ project/src/test/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrapTest.kt
@@ -1,3 +1,29 @@
 package dev.enderman.minecraft.plugins.template.bootstrap
 
-class TemplatePluginBootstrapTest {}
+import dev.enderman.minecraft.plugins.template.TemplatePlugin
+import org.mockbukkit.mockbukkit.MockBukkit
+import org.mockbukkit.mockbukkit.ServerMock
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class TemplatePluginBootstrapTest {
+
+    private lateinit var server: ServerMock
+    private lateinit var plugin: TemplatePlugin
+
+    @BeforeTest fun setUp() {
+        server = MockBukkit.mock()
+        plugin = MockBukkit.loadWith(TemplatePlugin::class.java, "paper-plugin.yml")
+    }
+
+    @Test fun onEnableTest() {
+        assertNotNull(server)
+        assertNotNull(plugin)
+    }
+
+    @AfterTest fun tearDown() {
+        MockBukkit.unmock()
+    }
+}
