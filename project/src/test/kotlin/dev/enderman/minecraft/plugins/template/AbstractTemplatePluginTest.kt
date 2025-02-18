a83491e3b5204f244a66189bddcc87c2730abfe4
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-18 09:54:59 +0000

    Reorganise tests

--- project/src/test/kotlin/dev/enderman/minecraft/plugins/template/AbstractTemplatePluginTest.kt
+++ project/src/test/kotlin/dev/enderman/minecraft/plugins/template/AbstractTemplatePluginTest.kt
@@ -7,20 +7,24 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class TemplatePluginTest {
-
-    private lateinit var server: ServerMock
-    private lateinit var plugin: TemplatePlugin
+abstract class AbstractTemplatePluginTest {
+    protected lateinit var server: ServerMock
+    protected lateinit var plugin: TemplatePlugin
 
     @BeforeTest fun setUp() {
         server = MockBukkit.mock()
         plugin = MockBukkit.loadWith(TemplatePlugin::class.java, "paper-plugin.yml")
+    }
 
+    @Test fun `server should be mocked correctly`() {
         MockBukkit.ensureMocking()
     }
 
-    @Test fun onEnableTest() {
+    @Test fun `server should start without errors`() {
         assertNotNull(server)
+    }
+
+    @Test fun `plugin should enable without errors`() {
         assertNotNull(plugin)
     }
 
--- project/src/test/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrapTest.kt
+++ project/src/test/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrapTest.kt
@@ -1,40 +1,27 @@
 package dev.enderman.minecraft.plugins.template.bootstrap
 
-import dev.enderman.minecraft.plugins.template.TemplatePlugin
+import dev.enderman.minecraft.plugins.template.AbstractTemplatePluginTest
 import io.mockk.mockk
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import org.mockbukkit.mockbukkit.MockBukkit
-import org.mockbukkit.mockbukkit.ServerMock
-import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class TemplatePluginBootstrapTest {
-
-    private lateinit var server: ServerMock
-    private lateinit var plugin: TemplatePlugin
+class TemplatePluginBootstrapTest : AbstractTemplatePluginTest() {
     private lateinit var pluginBootstrap: TemplatePluginBootstrap
 
-    @BeforeTest fun setUp() {
-        server = MockBukkit.mock()
-        plugin = MockBukkit.loadWith(TemplatePlugin::class.java, "paper-plugin.yml")
+    @BeforeTest fun initializeBootstrap() {
         pluginBootstrap = TemplatePluginBootstrap()
-
-        MockBukkit.ensureMocking()
     }
 
-    @Test fun bootstrapTest() {
-        assertNotNull(server)
-        assertNotNull(plugin)
+    @Test fun `plugin bootstrap should exit`() {
         assertNotNull(pluginBootstrap)
+    }
 
+    @Test fun `bootstrap method should work`() {
         val contextMock = mockk<BootstrapContext>()
 
         pluginBootstrap.bootstrap(contextMock)
-    }
-
-    @AfterTest fun tearDown() {
-        MockBukkit.unmock()
     }
 }
