422b8099a99bb8c163a5a688b7ccec7747940726
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-01 19:32:57 +0000

    Log command information (/about) on startup

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/template/TemplatePlugin.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/template/TemplatePlugin.kt
@@ -1,5 +1,7 @@
 package dev.enderman.minecraft.plugins.template
 
+import org.bukkit.command.Command
+import org.bukkit.command.defaults.VersionCommand
 import org.bukkit.plugin.java.JavaPlugin
 
 open class TemplatePlugin : JavaPlugin() {
@@ -11,7 +13,15 @@ open class TemplatePlugin : JavaPlugin() {
     }
 
     override fun onEnable() {
-        logger.info("Running version ${pluginMeta.version} for Minecraft ${pluginMeta.apiVersion} by ${pluginMeta.authors.joinToString(", ")}")
+        var versionCommand: Command? = null
+
+        server.commandMap.knownCommands.forEach { entry -> if (entry.value is VersionCommand) versionCommand = entry.value }
+
+        versionCommand?.execute(
+            server.consoleSender,
+            versionCommand!!.name,
+            arrayOf(pluginMeta.name)
+        )
 
         logger.info("Plugin has been fully enabled")
     }
