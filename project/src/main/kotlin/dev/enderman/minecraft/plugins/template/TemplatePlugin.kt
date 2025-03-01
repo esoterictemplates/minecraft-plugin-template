9c73ab35e15c73b88031346458a488f180f7a981
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-01 16:47:48 +0000

    Display logs upon plugin startup

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/template/TemplatePlugin.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/template/TemplatePlugin.kt
@@ -3,7 +3,16 @@ package dev.enderman.minecraft.plugins.template
 import org.bukkit.plugin.java.JavaPlugin
 
 open class TemplatePlugin : JavaPlugin() {
-    override fun onEnable() {
 
+    init {
+        logger.info("Initialising plugin...")
+
+        logger.info("Initialised plugin")
+    }
+
+    override fun onEnable() {
+        logger.info("Running version ${pluginMeta.version} for Minecraft ${pluginMeta.apiVersion} by ${pluginMeta.authors.joinToString(", ")}")
+
+        logger.info("Plugin has been fully enabled")
     }
 }
--- project/src/main/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrap.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/template/bootstrap/TemplatePluginBootstrap.kt
@@ -5,6 +5,7 @@ import io.papermc.paper.plugin.bootstrap.PluginBootstrap
 
 class TemplatePluginBootstrap : PluginBootstrap {
     override fun bootstrap(context: BootstrapContext) {
-
+        context.logger.info("Bootstrapping plugin...")
+        context.logger.info("Bootstrapping complete")
     }
 }
